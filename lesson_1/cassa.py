5e34d4892b3c960020b1cd0e11a26132a9e9c9f7
LamBust <olegoodno@gmail.com>
2025-03-20 20:08:20 +0300

    cassa ver. 0.3

--- lesson_1/cassa.py
+++ lesson_1/cassa.py
@@ -25,7 +25,7 @@ class Cassa():
     def showProducts(self):
         for i in range(1, len(self.products)+1):
             product = list(self.products)[i-1]
-            print(i, '. ', product, ' - ', self.products[product]*(1-self.discount[product]), ' Руб.', sep='')
+            print(i, '. ', product, ' - ', round(self.products[product]*(1-self.discount[product]), 2), ' Руб.', sep='')
             if self.discount[product] > 0:
                 print('СКИДКА:', str(self.discount[product]*100) + '%')
                 print('Цена без скидки -', self.products[product])
@@ -46,27 +46,19 @@ class Cassa():
         print()
         return purchaseDict
 
-    def showPurchases(self):
-        x = 16
+    def doCheck(self):
         purchases = self.doPurchases()
         result = 0
-        print(f"{'Наименование':{x}}{'Количество':{x}}{'Цена':{x}}{'Стоимость':{x}}")
         for i in range(1, len(purchases)+1):
             about_product = dict()
             product = list(purchases)[i-1]
             discount = self.discount[product]
             ammount = purchases[product][0]
-            price = purchases[product][1]
-            offered_price = price*(1 - discount)
-            ammount_price = purchases[product][2]
-            offered_ammount_price = ammount_price*(1 - discount)
+            price = float(purchases[product][1])
+            offered_price = round(price*(1 - discount), 2)
+            ammount_price = float(purchases[product][2])
+            offered_ammount_price = round(ammount_price*(1 - discount), 2)
             result += offered_ammount_price
-            print(f"{product:<{x}}{ammount:<{x}}{offered_price:<{x}}{offered_ammount_price:<{x}}")
-            if discount > 0:
-                print('Скидка на товар -', str(discount*100) + '%')
-                print(f"Цена без скидки: {price}", end='    ')
-                print(f"Стоимость без скидки: {ammount_price}")
-            print('ИТОГО:', str(result) + 'Руб.')
             about_product['Товар'] = product
             about_product['Цена'] = price
             about_product['Скидка'] = discount
@@ -75,8 +67,22 @@ class Cassa():
             about_product['Сумма'] = offered_ammount_price
             about_product['Сумма без скидки'] = ammount_price
             self.check.append(about_product)
-
+    
+    def showPurchases(self):
+        x = 16
+        result = 0
+        print(f"{'Наименование':{x}}{'Количество':{x}}{'Цена-Руб.':{x}}{'Стоимость-Руб.ыыы':{x}}")
+        for i in range(len(self.check)):
+            purchases = self.check[i]
+            result += purchases['Цена со скидкой']
+            print(f"{purchases['Товар']:<{x}}{purchases['Количество']:<{x}}{purchases['Цена со скидкой']:<{x}}{purchases['Сумма']:<{x}}")
+            if purchases['Скидка'] > 0:
+                print('Скидка на товар -', str(purchases['Скидка']*100) + '%')
+                print(f"Цена без скидки: {purchases['Цена']}", end='    ')
+                print(f"Стоимость без скидки: {purchases['Сумма без скидки']}")
+        print('ИТОГО:', str(result) + '-Руб.')
 cassa = Cassa()
 cassa.showProducts()
+cassa.doCheck()
 cassa.showPurchases()
 print(cassa.check)
